d575f9c81b578b9924bdc7de1c1f21f554ddf9d4
Ugglr <carl.igelstrom@gmail.com>
2022-01-23 11:18:43 +0800

    progress dump. Support page mostly.

---
+++ components/BMCButton.tsx
@@ -0,0 +1,26 @@
+import { ReactElement } from "react";
+import Image from "next/image";
+
+import styles from "../styles/BMCButton.module.scss";
+
+function BMCButton(): ReactElement {
+  return (
+    <a
+      className={styles.base}
+      href="https://www.buymeacoffee.com/ugglr"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <Image
+        className={styles.image}
+        width={80}
+        height={80}
+        src="/svgs/bmc-new-btn-logo.svg"
+        alt="buymecofee"
+      />
+      <div className={styles.text}>Buy me a coffee</div>
+    </a>
+  );
+}
+
+export default BMCButton;
--- content/SupportProjectPageContent.tsx
+++ content/SupportProjectPageContent.tsx
@@ -1,9 +1,56 @@
 import { ReactElement } from "react";
+import BMCButton from "../components/BMCButton";
+import Hero from "../components/Hero";
+
+import styles from "../styles/SupportProject.module.scss";
+import rocketLottie from "../public/lotties/rocket_brown.json";
 
 function SupportProjectPageContent(): ReactElement {
+  const lottieOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: rocketLottie,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <div>
-      <h1>🥺</h1>
+      <Hero
+        {...{
+          title: "Want to support this project?!",
+          subtitle:
+            "Wow, if you do that's so awsome! Here's the different ways you can! 👇",
+          lottieOptions,
+        }}
+      />
+
+      <div className={styles.heroMargin} />
+
+      <section>
+        <div className={styles.section}>
+          <div className={styles.sectionLeft}>
+            <h1 className={styles.bmcSectionTitle}>You can buy me a coffee!</h1>
+            <h3 className={styles.bmcSectionSubtitle}>
+              Keeping myself caffeinated makes me not fall asleep, and that
+              means, I can write more code!
+            </h3>
+          </div>
+          <BMCButton />
+        </div>
+      </section>
+
+      <section>
+        <div className={styles.section}>
+          <div className={styles.sectionLeft}>
+            <h1 className={styles.bmcSectionTitle}>Contribute Code!</h1>
+            <h3 className={styles.bmcSectionSubtitle}>
+              I am just a single developer with a passion for coffee, if you are
+              a developer all contributions are welcome!
+            </h3>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
